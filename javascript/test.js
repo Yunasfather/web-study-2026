7ee2d6e364f4df19e68a0509881da94d6fa803aa
jinwoo <superb.jinwoo@gmail.com>
2026-05-20 21:05:31 +0900

    Add week 1 HTML review

---
+++ javascript/test.js
@@ -0,0 +1,40 @@
+function hellKorea() {
+  console.log("한국어를 공부합니다.");
+}
+
+hellKorea();
+
+const studyJavaScript = function js() {
+  console.log("자바스크립트 연습 중");
+};
+
+console.log(studyJavaScript());
+
+const coding = (code) => console.log("코딩 연습합니다");
+
+console.log(coding());
+
+function sayFood(name) {
+  console.log("오늘 먹을 음식은 " + name + "입니다");
+}
+
+sayFood("라멘");
+
+function multy(a, b) {
+  return a * b;
+}
+
+console.log(multy(4, 5));
+
+const div = function divs(a, b) {
+  return a / b;
+};
+console.log(div(67, 3));
+
+const tri = (a) => { return a * 3; }
+
+console.log(tri(5));
+
+const square = (a) => a * a;
+
+console.log(square(5));
