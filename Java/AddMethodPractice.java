aaabd5718f33c9d2ef9b945bc7774f3dac547239
jinwoo <superb.jinwoo@gmail.com>
2026-06-07 00:39:28 +0900

    Practice Java method and return

---
+++ Java/AddMethodPractice.java
@@ -0,0 +1,11 @@
+public class AddMethodPractice {
+    public static void main(String[] args) {
+        int result = add(100, 200);
+        System.out.println(result);
+        
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
