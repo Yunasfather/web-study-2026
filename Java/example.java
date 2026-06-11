2c86b324aa6691452e2f751e57b512fad7071464
jinwoo <superb.jinwoo@gmail.com>
2026-06-11 17:07:59 +0900

    Practice Java scanner if and loop

---
+++ Java/example.java
@@ -0,0 +1,103 @@
+import java.util.Scanner;
+
+public class example {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
+        // System.out.println("이름: ");
+        // String name = sc.nextLine();
+
+        //  System.out.println("나이: ");
+        //  int age = sc.nextInt();
+
+        // System.out.println("점수: ");
+        //  int score = sc.nextInt();
+
+        //  System.out.println("키: ");
+        //  float height = sc.nextFloat();
+
+        //  System.out.println("공부중: ");
+        //  boolean studying = sc.nextBoolean();
+
+        //  if (score >= 60) {
+        //     System.out.println("합격");
+        //  }else {
+        //     System.out.println("불합격");
+        //  }
+
+
+        // System.out.println("==== 자기소개 ==== ");
+        //   System.out.println("이름 : " + name);
+        //   System.out.println("나이 : " + age);
+        //   System.out.println("점수 : " + score);
+        //   System.out.println("키 : " + height);
+        //   System.out.println("공부중 : " + studying);
+          
+        //   sc.close();
+//         System.out.println("연산기호: ");
+//   String oper = sc.nextLine();
+
+//    System.out.println("숫자 1: ");
+//  int num1 = sc.nextInt();
+//          System.out.println("숫자 2: ");
+//        int num2 = sc.nextInt();
+
+
+
+    
+
+//     int sum = 0;
+
+  
+  
+
+//     switch (oper) {
+
+
+
+
+//         case "+":
+//             sum = num1 + num2;
+//             System.out.println("합: " + sum);
+//             break;
+//                    case "-":
+//             sum = num1 - num2;
+//             System.out.println("차: " + sum);
+//             break;
+//                    case "*":
+//             sum = num1 * num2;
+//             System.out.println("곱: " + sum);
+//             break;
+//                    case "/":
+//             sum = num1 / num2;
+//             System.out.println("몫: " + sum);
+//             break;
+
+//                    case "%":
+//             sum = num1 % num2;
+//             System.out.println("나머지: " + sum);
+//             break;
+    
+//         default:
+//         System.out.println("제대로 입력하세요.");
+//             break;
+//     }
+//     if (num1 >= num2) {
+//         System.out.println("num1이 더 큽니다.");
+//     }else {
+//            System.out.println("num2가 더 크거나 같습니다.");
+//     }
+
+//     sc.close();
+
+    // for(int i = 1; i <= 10; i++) {
+    //     if(i % 2 == 0) {
+    //     System.out.println(i);
+    //     }
+    // }
+
+
+    }
+}
